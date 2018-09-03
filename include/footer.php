0bffa53117ae5880b5606126a5cc4979e46838c5
arigayas <arigayas+git@gmail.com>
2018-09-03 19:49:25 +0900

    Close #39

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 <?php if($o['type'] != 'index') {?>
-<div class="footer"><a href="<?php echo $o['Url']; ?>" style="text-decoration:underline"><?php echo $o['GlobalTitle']; ?></a> &gt; <?php echo $o['PageTitle']; ?>
+<div class="footer"><a href="<?php echo $o['Url']; ?>"><?php echo $o['GlobalTitle']; ?></a> &gt; <?php echo $o['PageTitle']; ?>
 <?php } ?>
 </div>
 </div><!-- /box -->
