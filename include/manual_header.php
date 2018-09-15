88f3d2877f696559a80ab51b94381b224f3ee309
arigayas <arigayas+git@gmail.com>
2018-09-15 20:10:37 +0900

    Close #37 Todo：背景画像に使っていた画像ファイルの削除

--- include/manual_header.php
+++ include/manual_header.php
@@ -1,5 +1,5 @@
 <div style="border:1px solid #e0e0e0;border-radius:5px">
-    <div class="font15" style="margin:20px 25px 40px 25px; line-height: 150%;text-align:left;background-image:url(<?php echo $o['Url']; ?>/img/music.png);background-repeat:no-repeat">
+    <div class="font15" style="margin:20px 25px 40px 25px; line-height: 150%;text-align:left;">
         <div style="margin-left:60px;margin-right:60px">
             <div class="font20" style="text-align:center;margin-bottom:40px;">
                 <h2 id="PageTitle"><?php echo $o['PageTitle']; ?></h2>
